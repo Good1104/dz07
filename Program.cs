583e0b590bf4786c97d0701f871629ec60bbcfdd
Good1104 <ilam43950@gmail.com>
2023-01-23 18:10:26 +0300

    7

---
+++ Program.cs
@@ -0,0 +1,149 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными 
+//вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+// Console.WriteLine("Введите количество строк");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int cols = Convert.ToInt32(Console.ReadLine());
+// int[,] array = new int[rows, cols];
+
+
+
+// FillArray(array);
+// PrintArray(array);
+
+
+// void FillArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             array[i, j] = new Random().Next(0, 1000);
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             Console.Write($"{array[i, j],3} \t");
+//         Console.WriteLine();
+//     }
+// }
+// Задача 50. Напишите программу, которая на вход принимает значение элемента в двумерном массиве,
+//  и возвращает позицию этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+// Console.WriteLine("Введите количество строк");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int cols = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(" введите элемент");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// int[,] array = new int[rows, cols];
+
+
+
+// FillArray(array);
+// PrintArray(array);
+// NumbersArray(array);
+
+// void FillArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             array[i, j] = new Random().Next(0, 1000);
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             Console.Write($"{array[i, j],3} \t");
+//         Console.WriteLine();
+//     }
+// }
+// void NumbersArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             if (array[i,j] == a)
+//             {
+//                 Console.WriteLine($"число {a} есть");
+//             }
+//             else Console.WriteLine($"число {a} нет");
+
+// }
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" введите элемент");
+int[,] array = new int[rows, cols];
+
+
+FillArray(array);
+PrintArray(array);
+SummColsArray(array);
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 1000);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3} \t");
+        Console.WriteLine();
+    }
+}
+void SummColsArray(int[,] array)
+{
+
+    int j = 0;
+    while (j < array.GetLength(1))
+    {int summ = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            summ = summ + array[i, j];
+
+        
+       
+        } Console.WriteLine($" SUMM {j} = {summ}");
+        
+    }   
+    j++;
+        
+
+    
+}
